bea4046b78558d4e61684ce87604fda8d51dba9b
Eslam Akram <eslam_akram91@yahoo.com>
2021-09-07 10:47:42 +0300

    updated

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -12,27 +12,23 @@ import React, { Component } from 'react'
     
       }
     
-      voting = () => {
-        this.setState({stateVote: this.state.stateVote + 1
-        })
+      handleClick = () => {
+        this.setState({stateVote: this.state.stateVote + 1 })
     
       }
     render() {
-        
-        
-        return (
+       
+                return (
 
-<div className="container">    
-        <h2> Title: {this.props.title}</h2>
+                 <div className="container">    
+                          <h2> Title: {this.props.title}</h2>
                           
-                <img  className={"beastImg"} src={`${this.props.imageRsc}`} alt={this.props.imgName +'  image'} onClick={this.voting} title={this.props.imageTitle}/>
+                          <img  className={"beastImg"} src={`${this.props.imageRsc}`} alt={this.props.imgName +'  image'} onClick={this.handleClick} title={this.props.imageTitle}/>
                 
-                <p> Description: {this.props.description}</p>
-                <p>Votes:{this.state.stateVote} <img src="https://www.vectorico.com/wp-content/uploads/2019/01/heart-icon-300x300.png" className="heartImg"></img></p>
+                            <p> Description: {this.props.description}</p>
+                              <p>Votes:{this.state.stateVote} <img src="https://www.vectorico.com/wp-content/uploads/2019/01/heart-icon-300x300.png" className="heartImg"></img></p>
 
-                {/* <button onClick = {this.voting}> <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsMli9qswhAuRZWqB1nENchOg3BASkqBDp9A&usqp=CAU'></img></button> */}
-
- </div>
+                </div>
         )
     }
 }
